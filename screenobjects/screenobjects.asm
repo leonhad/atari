46b04975bb8df64d2b0c8f526d1f95f07401d70c
Leonardo Costa <leonhad@gmail.com>
2019-11-12 21:45:15 -0300

    Fixed bugs

--- screenobjects/screenobjects.asm
+++ screenobjects/screenobjects.asm
@@ -2,6 +2,12 @@
     include "vcs.h"
     include "macro.h"
 
+; Start an unitialized segment at $80 for varable declaration
+    seg.u Variables
+    org $80
+P0Height ds 1       ; defines one byte for player 0 height
+P1Height ds 1       ; defines one byte for player 1 height
+
     seg
     org $F000
 
@@ -13,6 +19,10 @@ Reset:
 
     lda #%111        ; white playfield color
     sta COLUPF
+
+    lda #10         ; A = 10
+    sta P0Height    ; P0Height = 10
+    sta P1Height    ; P1Height = 10
 
 ; We set the TIA registers for the colors of P0 and P1.
     lda #$48        ; player 0 color light red
@@ -78,7 +88,7 @@ Player0Loop:
     sta GRP0
     sta WSYNC
     iny
-    cpy #10
+    cpy P0Height
     bne Player0Loop
 
     lda #0
@@ -92,7 +102,7 @@ Player1Loop:
     sta GRP1
     sta WSYNC
     iny
-    cpy #10
+    cpy P1Height
     bne Player1Loop
 
     lda #0
